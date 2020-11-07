38ffe3d3af27f26f34ad70df9600af1d82d1b833
xian <lopezalvarez.xian@gmail.com>
2020-11-07 12:15:40 +0000

    Function to draw bounding boxes

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 
 def keep_best_class(net_output):
@@ -16,3 +17,19 @@ def remove_background_predictions(predictions_nms, nclasses):
     for i in range(len(predictions_nms)):
         predictions.append(np.take(predictions_nms[i], predictions_nms[i][:, 4] != nclasses, axis=0))
     return predictions
+
+
+def draw_predictions(img, predictions):
+    # img: (height, width, 3)
+    # predictions: (num_preds_nms, 6) [xmin, ymin, width, height, class_id, conf]
+    height, width, _ = img.shape
+    color = (1.0, 0.0, 0.0)
+    for i in range(predictions.shape[0]):
+        box_xmin_abs = min(max(int(round(predictions[i, 0] * width)), 0), width - 2)
+        box_ymin_abs = min(max(int(round(predictions[i, 1] * height)), 0), height - 2)
+        box_width_abs = max(int(round(predictions[i, 2] * width)), 1)
+        box_height_abs = max(int(round(predictions[i, 3] * height)), 1)
+        box_xmax_abs = min(box_xmin_abs + box_width_abs, width - 1)
+        box_ymax_abs = min(box_ymin_abs + box_height_abs, height - 1)
+        img = cv2.rectangle(img, (box_xmin_abs, box_ymin_abs), (box_xmax_abs, box_ymax_abs), color, thickness=2)
+    return img
