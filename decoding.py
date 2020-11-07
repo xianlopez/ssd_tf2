bf71eb98e416c96e7f7aa8c816cbe3e747815fff
xian <lopezalvarez.xian@gmail.com>
2020-11-07 19:00:48 +0000

    Decoding predictions and GT

--- decoding.py
+++ decoding.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-max_detections_per_image = 50
+import tools
+from non_maximum_suppression import batch_non_maximum_suppression_fast
 
 
 def decode_box(box_enc, anchor):
@@ -39,28 +39,60 @@ def decode_box(box_enc, anchor):
     return xmin, ymin, width, height
 
 
-def decode_preds(net_output, anchors, th_conf):
-    # net_output: (nanchors, 4 + nclasses + 1)
-    #       [xc_enc, yc_enc, width_enc, height_enc, prob_class1, ..., prob_classN, prob_background]
-    # anchors: (nanchors, 4) [xmin, ymin, width, height]
-    # Note we don't consider the batch dimension here.
-    classes_probs = net_output[:, 4:-1]
-    max_positive_conf_all_boxes = np.max(classes_probs, axis=1)
-    sorted_indexes = np.argsort(-max_positive_conf_all_boxes)
-    indices_keep = sorted_indexes[:max_detections_per_image]
-    predictions = []
-    for idx in indices_keep:
-        if max_positive_conf_all_boxes[idx] > th_conf:
-            xmin, ymin, width, height = decode_box(net_output[idx, :4], anchors[idx, :])
-            class_id = np.argmax(classes_probs[idx, :])
-            box = PredictedBox([xmin, ymin, width, height], class_id, max_positive_conf_all_boxes[idx])
-            predictions.append(box)
+def decode_preds(net_output, anchors, nclasses):
+    # net_output:  (batch_size, nanchors, 4 + nclasses + 1)
+    coords_enc = net_output[:, :, :4]
+    coords_dec = decode_coords_batch(coords_enc, anchors)  # (batch_size, nanchors, 4)
+    net_output_dec = np.concatenate([coords_dec, net_output[:, :, 4:]], axis=-1)
+    predictions_full = tools.keep_best_class(net_output_dec)  # (batch_size, nanchors, 6)
+    predictions_nms = batch_non_maximum_suppression_fast(predictions_full, nclasses)
+    predictions = tools.remove_background_predictions(predictions_nms, nclasses)
     return predictions
+
+
+def decode_coords_batch(boxes_enc, anchors):
+    # boxes_enc: (batch_size, nanchors, 4) [xc_enc, yc_enc, width_enc, height_enc]
+    # anchors: (nanchors, 4) [xmin, ymin, width, height]
+
+    assert len(boxes_enc.shape) == 3
+
+    anchors_exp = np.expand_dims(anchors, axis=0)  # (1, nanchors, 4)
+
+    xc_enc = boxes_enc[..., 0]  # (batch_size, nanchors)
+    yc_enc = boxes_enc[..., 1]
+    w_enc = boxes_enc[..., 2]
+    h_enc = boxes_enc[..., 3]
+
+    # Take into account the 'variances':
+    xc_enc *= 0.1
+    yc_enc *= 0.1
+    w_enc *= 0.2
+    h_enc *= 0.2
+
+    anchor_xc = anchors_exp[..., 0] + anchors_exp[..., 2] / 2.0
+    anchor_yc = anchors_exp[..., 1] + anchors_exp[..., 3] / 2.0
+    anchor_width = anchors_exp[..., 2]
+    anchor_height = anchors_exp[..., 3]  # (1, nanchors)
+
+    xc = xc_enc * anchor_width + anchor_xc
+    yc = yc_enc * anchor_height + anchor_yc
+    width = np.exp(w_enc) * anchor_width
+    height = np.exp(h_enc) * anchor_height
+    xmin = xc - width / 2.0
+    ymin = yc - height / 2.0
+
+    boxes_dec = np.stack([xmin, ymin, width, height], axis=-1)
+
+    assert boxes_dec.shape == boxes_enc.shape
+
+    return boxes_dec  # (batch_size, nanchors, 4) [xmin, ymin, width, height]
 
 
 def decode_coords(boxes_enc, anchors):
     # boxes_enc: (nanchors, 4) [xc_enc, yc_enc, width_enc, height_enc]
     # anchors: (nanchors, 4) [xmin, ymin, width, height]
+
+    assert len(boxes_enc.shape) == 2
 
     xc_enc = boxes_enc[:, 0]
     yc_enc = boxes_enc[:, 1]
@@ -88,3 +120,32 @@ def decode_coords(boxes_enc, anchors):
     boxes_dec = np.stack([xmin, ymin, width, height], axis=1)
 
     return boxes_dec  # (nanchors, 4) [xmin, ymin, width, height]
+
+
+def decode_gt_batch(batch_gt, anchors):
+    # batch_gt: (batch_size, nanchors, 4 + nclasses + 1)
+    # [xmin_enc, ymin_enc, width_enc, height_enc, class1, ..., classN, background]
+    # anchors: (nanchors, 4) [xmin, ymin, width, height]
+    coords_enc = batch_gt[:, :, :4]
+    coords_dec = decode_coords_batch(coords_enc, anchors)  # (batch_size, nanchors, 4)
+    class_id = np.expand_dims(np.argmax(batch_gt[:, :, 4:], axis=-1), axis=-1)  # (batch_size, nanchors, 1)
+    gt_dec = np.concatenate([coords_dec, class_id], axis=-1)
+    nclasses = batch_gt.shape[2] - 5
+    positive_gt = []
+    for i in range(gt_dec.shape[0]):
+        positive_gt_i = np.take(gt_dec[i, :, :], np.where(gt_dec[i, :, 4] != nclasses)[0], axis=0)  # (ngt_i, 5)
+        # print('ngt_i = ' + str(positive_gt_i.shape[0]))
+        # Remove duplicated:
+        positive_gt_i_reduced = []
+        for j in range(positive_gt_i.shape[0]):
+            new_box = positive_gt_i[j, :]
+            repeated = False
+            for box in positive_gt_i_reduced:
+                if np.max(np.abs(box[:4] - new_box[:4])) < 0.0001:
+                    repeated = True
+                    break
+            if not repeated:
+                positive_gt_i_reduced.append(new_box)
+        # print('ngt_i_reduced = ' + str(len(positive_gt_i_reduced)))
+        positive_gt.append(np.array(positive_gt_i_reduced, np.float32))
+    return positive_gt  # list with elements of shape (ngt_i_reduced, 5)
