1d3190bc6da89d6cc1965d5ea6e979bc09c66112
xian <lopezalvarez.xian@gmail.com>
2020-11-16 17:16:54 +0000

    Adding argparse to evaluation script

--- evaluate.py
+++ evaluate.py
@@ -3,9 +3,7 @@ import random
 import tensorflow as tf
 from sys import stdout
 from datetime import datetime
-
-random.seed(0)
-np.random.seed(0)
+import argparse
 
 from parallel_reading import AsyncParallelReader, ReaderOpts
 from loss import SSDLoss
@@ -16,65 +14,76 @@ import drawing
 
 nclasses = 20
 img_size = 300
-
-model = build_model()
-load_vgg16_weigths(model)
-model.build((None, img_size, img_size, 3))
-model.summary()
-
-anchors = build_anchors(model)
-
-loss = SSDLoss()
-
-model.compile(loss=loss)
-
 voc_path = '/home/xian/datasets/VOCdevkit'
-batch_size = 32
-nworkers = 8
-
-period_batches_display = 5
 
 
 @tf.function
-def val_step_fum(batch_imgs, batch_gt):
+def val_step_fun(batch_imgs, batch_gt):
     net_output = model(batch_imgs, training=True)
     loss_value = loss(batch_gt, net_output)
     loss_value += sum(model.losses)
     return loss_value, net_output
 
 
-checkpoint = tf.train.Checkpoint(model=model)
+def evaluation_loop(val_reader, period_display):
+    print('Running evaluation')
+    evaluation_start = datetime.now()
+    val_loss = 0.0
+    all_batches_gt_raw = []
+    all_batches_net_output = []
+    for batch_idx in range(val_reader.nbatches):
+        batch_imgs, batch_gt, batch_gt_raw, names = val_reader.get_batch()
+        loss_value, net_output = val_step_fun(batch_imgs, batch_gt)
+        val_loss += loss_value
 
-checkpoint_to_load = 'ckpts/ckpt_89'
-read_result = checkpoint.read(checkpoint_to_load)
-read_result.assert_existing_objects_matched()
+        all_batches_gt_raw.append(batch_gt_raw)
+        all_batches_net_output.append(net_output.numpy())
 
-reader_ops = ReaderOpts(voc_path, nclasses, anchors, img_size, batch_size, nworkers)
-with AsyncParallelReader(reader_ops, 'val') as val_reader:
-        print('Running evaluation')
-        evaluation_start = datetime.now()
-        val_loss = 0.0
-        all_batches_gt_raw = []
-        all_batches_net_output = []
-        for batch_idx in range(val_reader.nbatches):
-            batch_imgs, batch_gt, batch_gt_raw, names = val_reader.get_batch()
-            loss_value, net_output = val_step_fum(batch_imgs, batch_gt)
-            val_loss += loss_value
+        stdout.write("\rbatch %d/%d, loss: %.2e    " %
+                     (batch_idx + 1, val_reader.nbatches, loss_value.numpy()))
+        stdout.flush()
 
-            all_batches_gt_raw.append(batch_gt_raw)
-            all_batches_net_output.append(net_output.numpy())
+        if (batch_idx + 1) % period_display == 0:
+            drawing.display_gt_and_preds(net_output, batch_imgs, batch_gt_raw, batch_gt, anchors, nclasses)
+    stdout.write('\n')
+    val_loss /= float(val_reader.nbatches)
+    mAP = mean_ap.compute_map(all_batches_net_output, all_batches_gt_raw, anchors)
+    print('mAP: %.4f' % mAP)
+    print('loss: %.4e' % val_loss)
+    print('Evaluation computed in ' + str(datetime.now() - evaluation_start))
 
-            stdout.write("\rbatch %d/%d, loss: %.2e    " %
-                         (batch_idx + 1, val_reader.nbatches, loss_value.numpy()))
-            stdout.flush()
 
-            if (batch_idx + 1) % period_batches_display == 0:
-                drawing.display_gt_and_preds(net_output, batch_imgs, batch_gt_raw, batch_gt, anchors, nclasses)
-        stdout.write('\n')
-        val_loss /= float(val_reader.nbatches)
-        mAP = mean_ap.compute_map(all_batches_net_output, all_batches_gt_raw, anchors)
-        print('mAP: %.4f' % mAP)
-        print('loss: %.4e' % val_loss)
-        print('Evaluation computed in ' + str(datetime.now() - evaluation_start))
+def evaluate(model, ckpt_idx, period_display, nworkers, batch_size):
+    checkpoint = tf.train.Checkpoint(model=model)
 
+    checkpoint_to_load = 'ckpts/ckpt_' + str(ckpt_idx)
+    read_result = checkpoint.read(checkpoint_to_load)
+    read_result.assert_existing_objects_matched()
+
+    reader_opts = ReaderOpts(voc_path, nclasses, anchors, img_size, batch_size, nworkers)
+    with AsyncParallelReader(reader_opts, 'val') as val_reader:
+        evaluation_loop(val_reader, period_display)
+
+
+if __name__ == '__main__':
+    random.seed(0)
+    np.random.seed(0)
+
+    parser = argparse.ArgumentParser(description='Evaluate a network')
+    parser.add_argument('--ckpt_idx', required=True, help='Index of the checkpoint to load')
+    parser.add_argument('--nworkers', default=8, help='number of processes to read data')
+    parser.add_argument('--batch_size', default=32)
+    parser.add_argument('--period_display', default=10, help='number of batches between to consecutive displays')
+    args = parser.parse_args()
+
+    model = build_model()
+    load_vgg16_weigths(model)
+    model.build((None, img_size, img_size, 3))
+    model.summary()
+    anchors = build_anchors(model)
+    loss = SSDLoss()
+    model.compile(loss=loss)
+
+    evaluate(model, int(args.ckpt_idx), int(args.period_display),
+             int(args.nworkers), int(args.batch_size))
 
